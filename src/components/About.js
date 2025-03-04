209e9d85f1eb8df369147ba6ed4c708a614876bb
Hari om <hariom@Haris-MacBook-Air.local>
2025-03-04 19:30:44 +0530

    added footer

--- src/components/About.js
+++ src/components/About.js
@@ -4,52 +4,51 @@ import mypic2 from "./mypic2.jpg";
 export default function About() {
   return (
     <div className="about">
+      <div className="left">
+        <h1>About Me</h1>
+        <p>
+          Hello! I’m Hari Om, a passionate and dedicated full stack developer
+          with over 3 years of experience in building dynamic and responsive web
+          applications. My expertise spans across both front-end and back-end
+          technologies, allowing me to create seamless and efficient user
+          experiences.
+        </p>
+        <p>
+          On the front-end, I specialize in HTML, CSS, JavaScript, and modern
+          frameworks and libraries like React and Angular. I enjoy crafting
+          intuitive and visually appealing interfaces that enhance user
+          engagement and satisfaction.
+        </p>
+        <p>
+          On the back-end, I have extensive experience with Python, Node.js,
+          Express, and databases such as MongoDB and SQL. I excel in designing
+          robust and scalable server-side applications, ensuring optimal
+          performance and security.
+        </p>
+        <p>
+          Currently I am working as 'Senior Software Engineer' at LTIMindtree.
+          My current assignment is a generative AI application, where users can
+          select models and create genAI application of their choice as per
+          their need.
+        </p>
+        <p>
+          I thrive in collaborative environments and have a proven track record
+          of working effectively with cross-functional teams to deliver
+          high-quality projects on time. My problem-solving skills and attention
+          to detail enable me to tackle complex challenges and continuously
+          improve my coding practices.
+        </p>
+        <p>
+          When I’m not coding, you can find me exploring the latest tech trends,
+          contributing to open-source projects, or enjoying outdoor activities
+          like hiking and cycling, and sometimes gaming.
+        </p>
+        <p>Let’s connect and create something amazing together!</p>
+      </div>
       <div className="right">
         <div className="image-container">
           <img id="dp" src={mypic2} alt="" />
         </div>
-      </div>
-      <div className="left">
-        <h1>About Me</h1>
-        <p>
-          Hi, I m Hari Om a Full stack Web Developer, Freelancer and most
-          importantly a Gamer in Free Time😁.
-        </p>
-        <p>
-          Currently I am working as 'Senior SOftware Engineer' at LTIMindtree
-        </p>
-        <p>
-          I love to learn new things, technologies which might help in solving
-          day to day problems and motivate the society to do good and be good to
-          others.
-        </p>
-        <p>
-          When free I prefer to explore new places taking unexplored ways just
-          to seek adventure😂
-        </p>
-        <p>
-          Coming back to my curriculumn, I garduated in 2022, from NIT Durgapur.
-          I completed my Intermediate from 'St. Michael's high school, Patna'
-          and Matriculation from 'St. Mary's Academy, Patna'.
-        </p>
-        <p>
-          And now along with the job that I am doing I am looking for better
-          oppertunities to learn, develop and grow mutually with the firm or
-          client
-        </p>
-        {/* <p>
-        So first back in 2020 I initially took admission in a college, and
-        decided to drop-out after a month, to do study on my own, to do
-        something really big and to chase my dreams in my own way, this was a
-        risk I know, but it started to work slowly and im learning new and
-        latest technologies from internet, not the 60 years old stuff from
-        college(pun intended). I'm not saying college is bad or like that. This
-        is way i took and these are my personal openion.
-      </p> */}
-        {/* <p>
-        Today I'm a Full Stack Web Developer with a 50k+ Subscribers YouTube
-        channel and alot of freelance projects.
-      </p> */}
       </div>
     </div>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { FaLinkedin, FaPhoneAlt } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-linkedin">
+        <FaLinkedin />
+        <p>https://www.linkedin.com/harry-99</p>
+      </div>
+      <div className="footer-contact">
+        <FaPhoneAlt />
+        <p>6207929491</p>
+      </div>
+      <div className="footer-email">
+        <MdEmail />
+        <p>hariom.ravi@gmail.com</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
